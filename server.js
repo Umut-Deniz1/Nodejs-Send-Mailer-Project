564a45af3366e534f8e3a4d699fd81921fc460a9
Umut <umutdeniz609@gmail.com>
2020-12-17 16:35:57 +0300

    update css and index.html files

--- server.js
+++ server.js
@@ -4,34 +4,34 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(express.static(__dirname + '/'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 var transporter = nodemailer.createTransport({
     port: 8080,
     service: 'gmail',
     auth: {
-      user: 'sendmailer005@gmail.com',
-      pass: 'Mailgonder'
+        user: 'sendmailer005@gmail.com',
+        pass: 'Mailgonder'
     }
-  })
-function sendEmail(mail){
+})
+function sendEmail(mail) {
     var mailOptions = {
         from: 'sendmailer005@gmail.com',
         to: mail.to,
         subject: mail.subject,
         html: mail.body,
     }
-    transporter.sendMail(mailOptions, function(err, info){
-        if(err){
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
             console.log(err)
         }
-        else{
+        else {
             console.log('Mesaj Gönderildi.' + info.response)
         }
     })
 }
 
-app.post('/sendEmailForm', (req,res) => {
+app.post('/sendEmailForm', (req, res) => {
     mail = {
         to: req.body.email,
         subject: 'Kayıt Doğrulama.',
